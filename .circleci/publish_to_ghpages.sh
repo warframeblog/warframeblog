540d3dec471881c0f99ab73f368e2615f80032ea
pods13 <alta0815@yandex.ru>
2018-07-14 21:02:42 +0300

    Remove redundant check

--- .circleci/publish_to_ghpages.sh
+++ .circleci/publish_to_ghpages.sh
@@ -4,11 +4,7 @@ DIR=$(dirname "$0")
 
 cd $DIR/..
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+git --version
 
 echo "Deleting old publication"
 rm -rf public
