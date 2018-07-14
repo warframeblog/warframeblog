bf9363719ca4560bc2aed8267e2ca0d34cd28778
pods13 <alta0815@yandex.ru>
2018-07-14 23:14:06 +0300

    Skip checking if dir changes

--- .circleci/publish_to_ghpages.sh
+++ .circleci/publish_to_ghpages.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-	git status
-    exit 1;
-fi
+# if [[ $(git status -s) ]]
+# then
+#     echo "The working directory is dirty. Please commit any pending changes."
+# 	git status
+#     exit 1;
+# fi
 
 echo "Deleting old publication"
 rm -rf public
