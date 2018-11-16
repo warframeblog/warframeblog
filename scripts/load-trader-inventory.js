9a0baa7f8b49c028ab4ccd6cfeb005f394d3b5bb
pods13 <alta0815@yandex.ru>
2018-11-16 20:48:02 +0300

    Init version of script to load void trader inventory

---
+++ scripts/load-trader-inventory.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const join = require('path').join;
+
+const API_URL = `https://api.warframestat.us/platform/voidTrader`;
+const FRAME_FOLDER = join(__dirname, '../static/trader');
