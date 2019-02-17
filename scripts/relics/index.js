a5db71c4173ead399328d9dd9d936f579013991b
pods13 <alta0815@yandex.ru>
2019-02-17 15:00:58 +0300

    Remove dependency on unvaulted relics

--- scripts/relics/index.js
+++ scripts/relics/index.js
@@ -1,38 +1,27 @@
 const _ = require('lodash');
 const relicsByRewards = require('../../data/relics/relicsRewards');
+const availableRelics = require('../../data/relics/availableRelics');
+const unavailableRelics = require('../../data/relics/unavailableRelics');
 const cetusRelics = require('../../data/relics/cetusRelics');
 const solarisRelics = require('../../data/relics/solarisRelics');
 const voidRelics = require('../../data/relics/voidRelics');
-const unvaultedRelics = require('../../data/relics/unvaultedRelics');
 const sanctuaryRelics = require('../../data/relics/sanctuaryRelics');
 
 const collectRelicsToItemParts = itemName => {
 	let relicsToItemParts = [];
 	_.each(relicsByRewards, (relicsRewards, relicName) => {
 		const relicRewardByItem = pickRelicRewardByItem(relicsRewards, itemName);
-		if(!relicRewardByItem) {
+		if(!relicRewardByItem || unavailableRelics.includes(relicName)) {
 			return;
 		}
 
 		relicsToItemParts.push({relic: relicName, itemPart: relicRewardByItem.name});
 	});
-	return hasUnvaultedRelics(relicsToItemParts) ? pickOnlyUnvaultedRelics(relicsToItemParts) : relicsToItemParts;
+	return relicsToItemParts;
 }
 
 const pickRelicRewardByItem = (relicsRewards, itemName) => {
 	return _.find(relicsRewards, relicItem => relicItem.name.includes(itemName));
-}
-
-const hasUnvaultedRelics = relicsToItemParts => {
-	return _.some(relicsToItemParts, isOneOfUnvaultedRelics);
-}
-
-const pickOnlyUnvaultedRelics = relicsToItemParts => {
-	return _.values(_.pickBy(relicsToItemParts, isOneOfUnvaultedRelics));
-}
-
-const isOneOfUnvaultedRelics = relicToItemPart => {
-	return unvaultedRelics.includes(relicToItemPart.relic);
 }
 
 
