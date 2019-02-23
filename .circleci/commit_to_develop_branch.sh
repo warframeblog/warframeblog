b527438fab237a51aa3fad9091d1d17708aa5105
pods13 <alta0815@yandex.ru>
2019-02-23 13:44:26 +0300

    Fix timestamp

--- .circleci/commit_to_develop_branch.sh
+++ .circleci/commit_to_develop_branch.sh
@@ -6,5 +6,5 @@ timestamp=$(date +%s%3N)
 echo "Updating trader inventory $timestamp"
 git status && \
   git add --all && \
-  git commit -m "Update trader inventory $timestamps" && \
+  git commit -m "Update trader inventory $timestamp" && \
   git push origin develop
--- .circleci/publish_to_ghpages.sh
+++ .circleci/publish_to_ghpages.sh
@@ -25,7 +25,7 @@ echo "Publishing version $timestamp"
 git status
 
 git add --all && \
-git commit -m "publish_to_ghpage $timestamps [ci skip]" && \
+git commit -m "publish_to_ghpage $timestamp" && \
 git tag "$timestamp" && \
 git push origin gh-pages && \
 git push origin "$timestamp"
