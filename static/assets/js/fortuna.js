bb12ce290ec5af18e39f723a5af359e184054066
pods13 <alta0815@yandex.ru>
2019-04-19 22:03:38 +0300

    Remove redundant scripts

--- static/assets/js/fortuna.js
+++ static/assets/js/fortuna.js
@@ -17,19 +17,4 @@ $(document).ready(function(){
     // add iframe src in, starting the video
     $iframe.attr('src',src);
   }
-
-  function selectFortunaFeature() {
-    $('.fortuna-features a[data-toggle="tab"]').click(function (e) {
-      e.preventDefault();
-      $('.fortuna-features li.active').removeClass('active');
-      $(this).parent().addClass('active');
-
-      var platformId = $(this).attr('href');
-      var activeTabClasses = "in active";
-      $('.fortuna-features-content .active').removeClass(activeTabClasses);
-      $(platformId).addClass(activeTabClasses);
-    });
-  }
-
-  selectFortunaFeature();
 });
