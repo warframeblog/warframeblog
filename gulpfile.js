789b8f7d285ab91e42eb2462ff2f7c4ca3348027
pods13 <alta0815@yandex.ru>
2019-04-26 23:13:14 +0300

    Update build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,75 +7,57 @@ const htmlmin = require('gulp-htmlmin');
 const autoprefixer = require('autoprefixer');
 const rename = require('gulp-rename');
 const postcss = require('gulp-postcss');
-const uncss = require('uncss');
+const uncss = require('postcss-uncss');
 const through = require('through2');
 const replace = require('gulp-replace');
-const csso = require('csso');
+const csso = require('postcss-csso');
 const gulpIgnore = require('gulp-ignore');
 
-const framePageStyles = 'static/assets/css/warframe-page';
-const stylesSource = 'src/styles/**/*.scss';
-const htmlSource = 'public/**/*.html';
-const stylesDestDirectory = 'static/assets/css';
+const scssFiles = 'src/styles/**/*.scss';
+const themeScssFiles = 'themes/hesti/src/css/**/*.scss';
+const styleAssets = 'static/assets/css';
+const htmlFilesToPublish = 'public/**/*.html';
+const styleFilesToPublish = 'public/assets/css/**/*.css';
+const publicStyleAssets = 'public/assets/css';
 
-gulp.task('clean:styles', function (done) {
-	rimraf.sync(framePageStyles);
-	done();
-});
-
-gulp.task('styles', gulp.series('clean:styles', () => {
-	return gulp.src(stylesSource)
+gulp.task('build:styles', () => {
+	return gulp.src(scssFiles)
 		.pipe(sass({
 			includePaths: ['node_modules', path.resolve('themes', 'hesti', 'src', 'css')]
 		}).on('error', sass.logError))
-		.pipe(postcss([ autoprefixer() ]))
 		.pipe(rename(function(file) {
 			if (file.basename === 'main') {
 				file.basename = 'hesti';
 			}
 		}))
-		.pipe(gulp.dest(stylesDestDirectory));	
-}));
-
-const siteRegex = /https:\/\/warframeblog\.com/g;
-
-gulp.task('inject:styles', () => {
-	let styles = '';
-	return gulp.src(htmlSource)
-		.pipe(gulpIgnore.include(function(vinylFile) {
-			return /link rel="stylesheet"/.test(vinylFile.contents.toString());
-		}))
-		.pipe(through.obj((vinylFile, enc, cb) => {
-			let transformedFile = vinylFile.clone();
-			const htmlContent = transformedFile.contents.toString().replace(siteRegex, '');
-			uncss(htmlContent, {htmlroot: 'public', ignore: [/.*\.ripple.*/, /.*\.dropdown-menu.*/, /.*\.show/, /.*\.toggled/, /.*\.nav-open.*/]}, function (error, output) {
-				if(error) {
-					console.log(`${error} - ${vinylFile.path}`);
-					cb(error);
-				}
-				transformedFile.styles = csso.minify(output, { comments: false }).css;
-				cb(null, transformedFile);
-			})
-		}))
-		.pipe(replace(/\<\!-- inject\:styles --\>.+\<\!-- endinject --\>/ms, function() {
-			console.log(`Injecting styles into ${this.file.path}`);
-			return `<style>${this.file.styles}</style>`;
-		})).on('error', console.log)
-		.pipe(gulp.dest('public'));	
+		.pipe(gulp.dest(styleAssets));	
 });
 
-gulp.task('minify:markup', () => {
-	return gulp.src(htmlSource)
+gulp.task('minify:styles', () => {
+	return gulp.src(styleFilesToPublish)
+		.pipe(postcss([uncss({htmlroot: 'public', html: ['public/**/*.html'], ignore: [/.*\.ripple.*/, /.*\.dropdown-menu.*/, /.*\.show/, /.*\.toggled/, /.*\.nav-open.*/]})]))
+		.pipe(postcss([ autoprefixer(), csso({ comments: false }) ]))
+		.pipe(gulp.dest(publicStyleAssets))
+});
+
+gulp.task('replace:html', () => {
+	return gulp.src(htmlFilesToPublish)
+		.pipe(replace('https://warframeblog.com/assets/', '/assets/'))
+		.pipe(gulp.dest('public'));
+});
+
+gulp.task('minify:html', () => {
+	return gulp.src(htmlFilesToPublish)
 		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(gulp.dest('public'));
 });
 
-gulp.task('hugo:build', shell.task('hugo'));
+gulp.task('build:hugo', shell.task('hugo'));
 
-gulp.task('build', gulp.series('styles', 'hugo:build', 'inject:styles', 'minify:markup'));
+gulp.task('build', gulp.series('build:styles', 'build:hugo', 'replace:html', 'minify:styles', 'minify:html'));
 
 gulp.task('watch', () => {
-	return gulp.watch(stylesSource, gulp.series('styles'));
+	return gulp.watch([scssFiles, themeScssFiles], gulp.series('build:styles'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'build:styles'));
