517f92e514ade258de44f92832daf7f9bc8ae6b1
pods13 <alta0815@yandex.ru>
2019-04-27 05:09:42 +0300

    Remove excess imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,12 @@
 // Bootstrap components
-import "bootstrap/js/src/alert";
 import "bootstrap/js/src/button";
 import "bootstrap/js/src/carousel";
 import "bootstrap/js/src/collapse";
-import "bootstrap/js/src/modal";
-import "bootstrap/js/src/popover";
-import "bootstrap/js/src/scrollspy";
 import "bootstrap/js/src/tab";
-import "bootstrap/js/src/tooltip";
 import "bootstrap/js/src/util";
 
 // Bootstrap Material Design components
-import "bootstrap-material-design/js/checkbox";
-import "bootstrap-material-design/js/checkboxInline";
 import "bootstrap-material-design/js/collapseInline";
-import "bootstrap-material-design/js/file";
-import "bootstrap-material-design/js/radio";
-import "bootstrap-material-design/js/radioInline";
-import "bootstrap-material-design/js/select";
-import "bootstrap-material-design/js/switch";
-import "bootstrap-material-design/js/text";
-import "bootstrap-material-design/js/textarea";
 import "bootstrap-material-design/js/dropdown";
 
 import "bootstrap-material-design/js/drawer";
